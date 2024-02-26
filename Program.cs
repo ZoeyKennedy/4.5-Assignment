0b9e967f6e3fb399501a08ba81d0ff251106a56e
ZoeyKennedy <160169473+ZoeyKennedy@users.noreply.github.com>
2024-02-26 12:13:23 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _4._5_Assignement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
